4d7c17cb1f8cb4706d4c78a5ccccd2990f9fe3f4
marla294@gmail.com <marla294@gmail.com>
2022-08-11 11:31:02 -0500

    Finishing up mapping row data to dataset

--- IngestionManager.cs
+++ IngestionManager.cs
@@ -2,7 +2,9 @@ namespace UploadCSVFile
 {
     public class IngestionManager
     {
-      public ColumnMappings mappings;
+      public ColumnMappings mappings = new ColumnMappings();
+
+      public char[] separators = { ',' };
 
       public void IngestDataset(string fileName) 
       {
@@ -20,18 +22,39 @@ namespace UploadCSVFile
           data.Add(row);
         }
 
-        //string line1Data = reader.ReadLine();
       }
 
       public Row PopulateRow(string currentLine) {
         Row row = new Row();
+
+        var data = currentLine.Split(separators);
+        int column = 0;
+
+        foreach (var cell in data) {
+          if (column == mappings.UserId) {
+            row.UserId = cell;
+          }
+          if (column == mappings.FirstName) {
+            row.FirstName = cell;
+          }
+          if (column == mappings.LastName) {
+            row.LastName = cell;
+          }
+          if (column == mappings.Version) {
+            row.Version = Int32.Parse(cell);
+          }
+          if (column == mappings.InsuranceCompany) {
+            row.InsuranceCompany = cell;
+          }
+
+          column++;
+        }
 
         return row;
       }
 
       public void SetColumnMappings(string header) 
       {
-        char[] separators = { ',' };
         var data = header.Split(separators);
         int column = 0;
 
